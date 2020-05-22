03d77e315756a8748723f7dd92e5ad659e47c8c2
dyushez <51192331+dyushez@users.noreply.github.com>
2020-05-23 02:15:07 +0300

    Update board.py

--- board.py
+++ board.py
@@ -1,10 +1,16 @@
 from arrays import Array2D
+from btree import BTree
+import random
+import copy
 
 
 class Board:
+    CLEAN = ' _'
     def __init__(self):
         self.field = Array2D(3, 3)
-        self.clear(' _')
+        self.clear(self.CLEAN)
+        self.last = ' x'
+        #self.tree = LinkedBinaryTree(self.field)
 
     def clear(self, value=None):
         self.field.clear(value)
@@ -34,7 +40,7 @@ class Board:
         def horizontal_check():
             for i in range(3):
                 if self.field[i, 0] == self.field[i, 1] == self.field[i, 2]:
-                    return f'{self.field[j, 0]} wins!'
+                    return f'{self.field[i, 0]} wins!'
                 else:
                     continue
             else:
@@ -50,26 +56,64 @@ class Board:
                 return None
 
         def is_full():
-            for i in range(3):
-                for j in range(3):
-                    position = [i, j]
-                    if self.is_available(position):
-                       return False
-            else:
+            if len(self.availables()) == 0:
                 return True
+            else:
+                return False
 
         if not is_full():
-            return diagonal1_check() or diagonal2_check() or \
+            res = diagonal1_check() or diagonal2_check() or \
                    horizontal_check() or vertical_check()
+            if res is None:
+                res = 'continue..'
+            return res
         else:
-            return 'friendship wins!'
+            return 'draw!'
 
-    def build_tree(self):
-        pass
+    def generate_position(self):
+        av = self.availables()
+        p = random.randint(0, len(av))
+        return av[p]
+
+    def build_tree(self, node):
+        if len(self.availables()) != 1:
+            self.laaast()
+            tree = BTree(self)
+
+            left_tree = copy.deepcopy(self)
+            lp = left_tree.generate_position()
+            left_tree.field[lp] = self.last
+            right_tree = copy.deepcopy(self)
+            rp = right_tree.generate_position()
+            right_tree.field[rp] = self.last
+
+            tree.insert_left(left_tree)
+            tree.insert_right(right_tree)
+
+            left_tree.build_tree()
+            right_tree.build_tree()
+        else:
+            left_tree = copy.deepcopy(self)
+            lp = left_tree.generate_position()
+            left_tree.field[lp] = self.last
+
+    def laaast(self):
+        if self.last == ' o':
+            self.last = ' x'
+        else:
+            self.last = ' o'
+
+    def availables(self):
+        av = []
+        for i in range(3):
+            for j in range(3):
+                if self.is_available((i,j)):
+                    av.append((i,j))
+        return av
 
     def is_available(self, position):
         if 0 <= position[0] <= 2 and 0 <= position[1] <= 2:
-            if self.field[position[0], position[1]] == ' _':
+            if self.field[position[0], position[1]] == self.CLEAN:
                 return True
             else:
                 return False
